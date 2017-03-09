7444ee3a25edc93ac28787aabb75a46af2f27341
yongyii <me_yee@hotmail.com>
2017-03-09 19:10:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "ยังไม่พร้อม"
+				'text' => $test."ยังไม่พร้อม"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
