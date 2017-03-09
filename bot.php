76dfc0cf3020b7b8343f9992d42f384ff1ba5c50
yongyii <me_yee@hotmail.com>
2017-03-09 16:16:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'ไม่พร้อมทำให้คับ' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
